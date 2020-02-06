8edbc1c350ca6812f6915e6c3a1e3ba3917e6c00
Christophe Priouzeau <christophe.priouzeau@st.com>
2020-02-06 18:11:07 +0100

    M4FWCOREDUMP: add management of historic
    Change-Id: If6ec3b343d4e4d725692a04f5cdf51af363d7a8d
    Signed-off-by: Arnaud Pouliquen <arnaud.pouliquen@st.com>
    Signed-off-by: Christophe Priouzeau <christophe.priouzeau@st.com>

--- recipes-extended/m4coredump/files/stm32mp-m4fwdump.sh
+++ recipes-extended/m4coredump/files/stm32mp-m4fwdump.sh
@@ -1,7 +1,29 @@
 #!/bin/sh
 
-mkdir -p /var/crash
+counter=1
+DIRECTORY=/var/crash
+DEPTH=5
+
+mkdir -p "$DIRECTORY"
+cd ${DIRECTORY}
+
+MAX_HISTORIC=$(ls -1 | wc -l)
+if [ $MAX_HISTORIC -gt $DEPTH ];
+then
+    #cleanup history
+    ls -1 | sort -g | head -n $(($MAX_HISTORIC -$DEPTH +1)) | xargs --no-run-if-empty rm
+fi
+
+#get counter value
+count=`ls -1 | sort -g | tail -n 1 | cut -d '_' -f 1`
+count=$(($count + 1))
+
+#register new dump
 timestamp=$(date +%F_%H-%M-%S)
-filename=/var/crash/m4-fw-error_${timestamp}.dump
+filename=${count}_m4-fw-error_${timestamp}.dump
 cat /sys/${DEVPATH}/data > ${filename}
 echo 1 > /sys/${DEVPATH}/data
+
+# synchronize filesystem
+sync
+cd -
