4f1d165844186dc28ad0a69863df2e6a01d71a54
Romuald JEANNE <romuald.jeanne@st.com>
2020-06-17 16:42:53 +0200

    WIFI: add support of new board
    Change-Id: I38d85327d6864fc6aa733770c4ceb78ea206a2d1

--- recipes-connectivity/wifi/wifi-suspend/wifi_brcmfmac_driver.sh
+++ recipes-connectivity/wifi/wifi-suspend/wifi_brcmfmac_driver.sh
@@ -1,6 +1,31 @@
 #!/bin/sh
 SDMMC_PATH=/sys/bus/amba/drivers/mmci-pl18x
 
+COMPATIBLE_BOARD=$(cat /proc/device-tree/compatible | sed "s|st,|,|g" | cut -d ',' -f2)
+
+case $COMPATIBLE_BOARD in
+stm32mp151a-dk2*)
+	# supported
+	;;
+stm32mp151f-dk2*)
+	# supported
+	;;
+stm32mp153a-dk2*)
+	# supported
+	;;
+stm32mp153f-dk2*)
+	# supported
+	;;
+stm32mp157c-dk2*)
+	# supported
+	;;
+stm32mp157f-dk2*)
+	# supported
+	;;
+	*)
+	exit 1;
+esac
+
 if [ -d $SDMMC_PATH ];
 then
 
