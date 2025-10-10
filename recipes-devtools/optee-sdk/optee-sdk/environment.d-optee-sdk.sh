23eb75737890f3b143579cad18e1598d139b6310
Christophe Priouzeau <christophe.priouzeau@foss.st.com>
2025-10-10 16:28:19 +0200

    OPTEE sdk: manage aarch32
    Signed-off-by: Christophe Priouzeau <christophe.priouzeau@foss.st.com>

--- recipes-devtools/optee-sdk/optee-sdk/environment.d-optee-sdk.sh
+++ recipes-devtools/optee-sdk/optee-sdk/environment.d-optee-sdk.sh
@@ -2,10 +2,14 @@ export TEEC_EXPORT=$SDKTARGETSYSROOT/usr
 if [ -d "$SDKTARGETSYSROOT/usr/include/optee/export-user_ta" ]; then
     export TA_DEV_KIT_DIR=$SDKTARGETSYSROOT/usr/include/optee/export-user_ta
 else
-    if [ -d "$SDKTARGETSYSROOT/usr/include/optee/export-user_ta_arm64" ]; then
-        export TA_DEV_KIT_DIR=$SDKTARGETSYSROOT/usr/include/optee/export-user_ta_arm64
+    if [ -d "$SDKTARGETSYSROOT/usr/include/optee/export-user_ta_arm32" ]; then
+        export TA_DEV_KIT_DIR=$SDKTARGETSYSROOT/usr/include/optee/export-user_ta_arm32
     else
-        echo "[SDK ERROR] Issue to optee export-user_ta directory"
+        if [ -d "$SDKTARGETSYSROOT/usr/include/optee/export-user_ta_arm64" ]; then
+            export TA_DEV_KIT_DIR=$SDKTARGETSYSROOT/usr/include/optee/export-user_ta_arm64
+        else
+            echo "[SDK ERROR] Issue to optee export-user_ta directory"
+        fi
     fi
 fi
 
