748221a4aef8cb58bd2a7f468f3a42f2839f18f9
Christophe Priouzeau <christophe.priouzeau@foss.st.com>
2026-04-29 11:43:26 +0200

    BOOT.SCR: adapt name for copro usage
    Signed-off-by: Christophe Priouzeau <christophe.priouzeau@foss.st.com>

--- recipes-bsp/u-boot/stm32mp-extlinux/boot.scr.cmd
+++ recipes-bsp/u-boot/stm32mp-extlinux/boot.scr.cmd
@@ -14,22 +14,22 @@
 
 echo "Executing SCRIPT on target=${target}"
 
-# M4 Firmware load
-env set m4fw_name "rproc-m4-fw.elf"
-env set m4fw_addr ${kernel_addr_r}
-env set boot_m4fw 'rproc init; rproc load 0 ${m4fw_addr} ${filesize}; rproc start 0'
+# Copro Firmware load
+env set coprofw_name "rproc-copro-fw.elf"
+env set coprofw_addr ${kernel_addr_r}
+env set boot_coprofw 'rproc init; rproc load 0 ${coprofw_addr} ${filesize}; rproc start 0'
 
-# boot M4 Firmware when available
-env set scan_m4fw 'if test -e ${devtype} ${devnum}:${distro_bootpart} ${m4fw_name};then echo Found M4 FW $m4fw_name; if load ${devtype} ${devnum}:${distro_bootpart} ${m4fw_addr} ${m4fw_name}; then run boot_m4fw; fi; fi;'
+# boot copro Firmware when available
+env set scan_coprofw 'if test -e ${devtype} ${devnum}:${distro_bootpart} ${coprofw_name};then echo Found COPRO FW ${coprofw_name}; if load ${devtype} ${devnum}:${distro_bootpart} ${coprofw_addr} ${coprofw_name}; then run boot_coprofw; fi; fi;'
 
 # management of overlay
 env set ov_init 'load ${devtype} ${devnum}:${distro_bootpart} ${fdt_addr_r} ${fdtfile} && env set fdt_addr ${fdt_addr_r} && fdt addr ${fdt_addr} && setexpr fdtovaddr ${fdt_addr} + C0000'
 env set ov_apply 'test -n ${fdtovaddr} && test -n ${overlay} && for ov in ${overlay}; do echo overlaying ${ov}...; load ${devtype} ${devnum}:${distro_bootpart} ${fdtovaddr} /overlays/${ov}.dtbo && fdt resize ${filesize} && fdt apply ${fdtovaddr}; done'
 env set scan_overlays 'if test -e ${devtype} ${devnum}:${distro_bootpart} /overlays/overlays.txt && load ${devtype} ${devnum}:${distro_bootpart} ${loadaddr} /overlays/overlays.txt && env import -t ${loadaddr} ${filesize}; then echo loaded overlay.txt: ${overlay}; run ov_init; run ov_apply; fi'
 
-# Update the DISTRO command to search in sub-directory and load M4 firmware
+# Update the DISTRO command to search in sub-directory and load copro firmware
 env set boot_prefixes "/${boot_device}${boot_instance}_"
-env set boot_extlinux "run scan_m4fw;run scan_overlays; ${boot_extlinux}"
+env set boot_extlinux "run scan_coprofw;run scan_overlays; ${boot_extlinux}"
 
 # save the boot config for the 2nd boot
 env set boot_targets ${target}
